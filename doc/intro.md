a49949fa5bd03d2acc58623fc7b80cc1756d522d
Cooper LeBrun <cooperlebrun@gmail.com>
2020-04-27 16:29:38 -0700

    lein new (default template)

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ideal_product_exporter
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ideal_product_exporter/core_test.clj
@@ -0,0 +1,7 @@
+(ns ideal-product-exporter.core-test
+  (:require [clojure.test :refer :all]
+            [ideal-product-exporter.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
