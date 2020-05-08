cb08a748ede2a980aac3b598fdc06779b21319df
Cooper LeBrun <cooperlebrun@gmail.com>
2020-05-08 11:01:22 -0700

    Remove unfamiliar clojure feature usage.
    Including:
        clojure.spec.alpha
        resumable exceptions
    
    I really want to use these features (spec > r. exceptions), but for now
    I have a job to do, and I need to get more experience with spec in
    trivial cases and toy code before I can try using it in production.

--- src/ideal_product_exporter/core.clj
+++ src/ideal_product_exporter/core.clj
@@ -1,8 +1,13 @@
 (ns ideal-product-exporter.core
-  (:require [clojure.data.csv   :as csv]
-            [clojure.java.io    :as io]
-            [clojure.spec.alpha :as s]
-            [clojure.string :as str]))
+  (:require [clojure.data.csv        :as csv]
+            [clojure.java.io         :as io]
+            [clojure.string          :as string]))
+            ;; [clojure.spec.alpha      :as s]
+            ;; [clojure.spec.test.alpha :as stest]
+
+;; NOTES
+; Should be as lazy as possible to avoid holding all data in memory at once to
+; avoid running out of memory when processing large csv files.
 
 ;;; UTIL
 
@@ -12,91 +17,35 @@
 
 (defn words [^String string]
   "Splits STRING on whitespace, returning vector of words."
-  (str/split string #"[\W]+"))
+  (string/split string #"[\W]+"))
 
 (defn keywordize [^String string]
   "Returns a keyword symbol of STRING in pascal case."
-  (->> (words string) (map str/capitalize) str/join keyword))
+  (->> (words string) (map string/capitalize) string/join keyword))
 
-;; RESUMABLE EXCEPTIONS
-;; Doing error handling through a condition system built on top of Clojure's
-;; dynamic binding system, as described here:
-;; https://www.youtube.com/watch?v=zp0OEDcAro0
+;;; SPEC
+;; Empty for now. Clearly I have a lot to learn about how spec works.
+;; I can easily create specs for data but the #1 use I had for it was
+;; instrumentation and property-based testing for functions, neither
+;; of which I can get working for now.
 
-(defmacro deferror
-  "Defines a dynamic function that takes a msg string and info map, and `throw``s.
-  Intended to be used as the foundation of a resumable exception system, instead of
-  traditional error handling. Using `binding` and other dynamic vars you can
-  define different ways of handling errors without breaking function
-  encapsulation or deciding on behalf of other callers how they want to handle
-  the error. For an overview of how this works, please see the talk 'Condition
-  Systems in an Exceptional Language' by Chris Houser."
-  [err-name]
-  (list 'defn (vary-meta err-name assoc :dynamic true) ['msg 'info]
-        '(throw (ex-info msg info))))
+;;; APPLICATION CODE
+(defn csv-data->pp-maps
+  "Turns csv/read-csv data (vec of vec of strings) into map of :valid and
+  :invalid rows. values are lazy seqs so you only compute what you use.
+  Optimized for memory: you only allocate/compute what you use."
+  [[header-data & rows]]
+  (let [header (map keywordize header-data)
+        header-length (count header) ; pre-compute
+        valid? #(= (count header) (count %))] ; remove rows that have a different number of cells from header
+    (map #(zipmap header %) (keep #(when (valid? %) %) rows))))
 
-(defmacro deferrorcase
-  "Defines a dynamic var with no value. Intended to use in resumable exception system."
-  [case-name]
-  (list 'def (vary-meta case-name assoc :dynamic true)))
+;; SPIKE
+(def spike-csv-filepath "/Users/cooperlebrun/RDP_Share/husq_ppl.TXT")
 
-;;; CODE
+(def spike-csv-data
+   (with-open [reader (io/reader spike-csv-filepath)]
+     (doall (csv/read-csv reader))))
 
-(deferror *invalid-product-record-file-error*)
-(deferror *invalid-product-file-error*)
-(deferrorcase *skip-value*)
-(deferrorcase *keep-value*)
-(deferrorcase *use-value*)
-
-(def string-or-number? #(or (juxt number? string?)))
-
-(s/def ::dollar-w-comma-str (s/and string? #(re-matches #"[\d,]+\.\d+" %)))
-(s/def ::csv-data-row (s/coll-of string?))
-(s/def ::csv-data (s/cat :header (s/? ::csv-data-row)
-                         :rows (s/* ::csv-data-row)))
-(s/def ::pp-header (s/coll-of keyword? :distinct true))
-(s/def ::pp-map (s/map-of keyword? string-or-number?))
-
-(s/fdef csv-row->pp-map
-  :args (s/alt :unary (s/cat :header ::pp-header)
-               :binary (s/cat :header ::pp-header :row ::csv-data-row))
-  :ret (s/alt :curried fn?
-              :applied ::pp-map)
-  :fn (s/and #(condp get-in %
-                [:ret :applied] ; function has been applied
-                (let [arg-rows (get-in % [:args :binary :rows])
-                      ret-rows (get-in % [:ret :applied])]
-                  (<= (count ret-rows) (count arg-rows))) ; return should be equal or shorter
-                true))) ; otherwise true
-(defn csv-row->pp-map
-  ([header] ;; partial
-   #(csv-row->pp-map header %))
-  ([header row]
-   (if (and (= (count header) (count row))) ; add validations to `and` form
-     (zipmap header row)
-     (binding [*keep-value* identity]
-       (*invalid-product-record-error*
-        "Row was not well formed, couldn't use."
-        {:data row})))))
-
-(s/fdef csv->pp-maps
-  :args ::csv-data
-  :ret (s/coll-of ::pp-map :distinct true)
-  :fn (fn [{args :args ret :ret}]
-        (<= (count ret) (count (:rows args)))))
-
-(defn csv->pp-maps [[header & rows]]
-  (let [row-parser (->> (map keywordize header) csv-row->pp-map)]
-    (keep #(binding [*skip-value* (constantly nil)] ; condition case. keep skips nil values.
-            row-parser %) rows)))
-
-
-(def test-csv-data (with-open [reader (io/reader "/Users/cooperlebrun/RDP_Share/husq_ppl.TXT")]
-                     (doall
-                      (csv/read-csv reader))))
-
-
-;; doesn't raise error
-;(binding [*invalid-product-record-error* *skip-value*] (csv->pp-maps test-csv-data))
-;; raises error
-;(parse-csv test-csv-data)
+(def spike-parsed-data
+  (csv-data->pp-maps [["header"] ["data"]]))
